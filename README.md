d3a9f006b8bc4b698bc75b1a9176efea8a194a68
Patrick Hofmann <patrickhofmanndesigns@gmail.com>
2021-10-26 21:41:22 -0500

    Updated README, made a little safer

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Abacus
+
+[![GitHub](https://img.shields.io/github/license/LazuriteMC/Abacus?color=A31F34&label=License&labelColor=8A8B8C)](https://github.com/LazuriteMC/Abacus/blob/main/LICENSE)
+[![Discord](https://img.shields.io/discord/719662192601071747?color=7289DA&label=Discord&labelColor=2C2F33&logo=Discord)](https://discord.gg/NNPPHN7b3P)
+
+The Lazurite Team's Gradle plugin. Requires [loom](https://github.com/FabricMC/fabric-loom) to be applied first.
+
+## Features
+
+When applied to the `settings.gradle`:
+
+- Adds an `abacus` extension with the following properties:
+
+    - `minecraftVersion`: Specifies the version of Minecraft to be applied as a `minecraft` dependency. See [here](https://fabricmc.net/versions.html) for version details.
+    - `fabricLoaderVersion`: Specifies the version of the Fabric Loader to be applied as a `modImplementation` dependency. See [here](https://fabricmc.net/versions.html) for version details.
+
+When applied to a `build.gradle`:
+
+- Sets `group` to `'dev.lazurite'`.
+- Sets `java.toolchain.languageVersion` to `JavaLanguageVersion.of(16)`.
+- Adds `java.withSourcesJar()`.
+- Adds `jar.from 'LICENSE'`.
+- Applies `mappings loom.officialMojangMappings()` as a dependency.
+- Applies `minecraft 'com.mojang:minecraft:VERSION'` as a dependency where `VERSION` is specified using the `abacus` extension from `settings.gradle`.
+- Applies `modImplementation 'net.fabricmc:fabric-loader:VERSION'` as  a dependency where `VERSION` is specified using the `abacus` extension from `settings.gradle`.
--- src/main/groovy/dev/lazurite/abacus/project/AbacusProjectPlugin.groovy
+++ src/main/groovy/dev/lazurite/abacus/project/AbacusProjectPlugin.groovy
@@ -2,7 +2,6 @@ package dev.lazurite.abacus.project
 
 import dev.lazurite.abacus.AbacusPlugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.PluginInstantiationException
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 
 final class AbacusProjectPlugin extends AbacusPlugin<Project> {
@@ -10,7 +9,7 @@ final class AbacusProjectPlugin extends AbacusPlugin<Project> {
     @Override
     void apply(Project target) {
         // abacus expects loom to be applied to the project first
-        if (!target.pluginManager.hasPlugin('fabric-loom')) throw new PluginInstantiationException('Plugin \'fabric-loom\' must be applied to project before \'dev.lazurite.abacus\'!')
+        if (!target.pluginManager.hasPlugin('fabric-loom')) throw new IllegalStateException('Plugin \'fabric-loom\' must be applied to project before \'dev.lazurite.abacus\'!')
 
         // register the project extension
         target.extensions.create('abacus', AbacusProjectExtension)
@@ -36,7 +35,11 @@ final class AbacusProjectPlugin extends AbacusPlugin<Project> {
 
         target.dependencies {
             mappings target.loom.officialMojangMappings()
+
+            if (ABACUS_PROPERTIES.get('minecraftVersion') == null) throw new IllegalStateException('\'minecraftVersion\' is null. Was it set in \'settings.gradle?\'')
             minecraft "com.mojang:minecraft:" + ABACUS_PROPERTIES.get('minecraftVersion').get()
+
+            if (ABACUS_PROPERTIES.get('fabricLoaderVersion') == null) throw new IllegalStateException('\'fabricLoaderVersion\' is null. Was it set in \'settings.gradle?\'')
             modImplementation "net.fabricmc:fabric-loader:" + ABACUS_PROPERTIES.get('fabricLoaderVersion').get()
         }
     }
